be7b6f3b044856b03cf309e2c7eb23df03bf4ed3
Ali Khadempour <ali.khadempour@outlook.com>
2024-09-17 13:34:39 +0330

    Access load and select options.

--- Presentation/Controllers/AccessController.cs
+++ Presentation/Controllers/AccessController.cs
@@ -1,6 +1,8 @@
 ﻿using AggregateVersions.Domain.Entities;
 using AggregateVersions.Domain.Interfaces;
+using AggregateVersions.Presentation.Models;
 using Microsoft.AspNetCore.Mvc;
+
 
 namespace AggregateVersions.Presentation.Controllers
 {
@@ -12,7 +14,36 @@ namespace AggregateVersions.Presentation.Controllers
 
             List<Access> accesses = await accessesService.GetAll();
 
-            return View(accesses);
+            return View(BuildJsTreeStructure(accesses));
+        }
+
+        private static List<AccessNode> BuildJsTreeStructure(List<Access> accessList)
+        {
+            Dictionary<long, AccessNode> nodeDictionary = [];
+
+            foreach (Access access in accessList)
+            {
+                nodeDictionary[access.ID] = new AccessNode
+                {
+                    ID = access.Guid.ToString(),
+                    DisplayName = access.Title ?? "Unnamed Access",
+                    Nodes = []
+                };
+            }
+
+            List<AccessNode> rootNodes = [];
+
+            foreach (Access access in accessList)
+            {
+                if (access.ParentId.HasValue && access.ParentId != 0 && nodeDictionary.TryGetValue(access.ParentId.Value, out AccessNode? parentNode))
+                {
+                    parentNode.Nodes?.Add(nodeDictionary[access.ID]);
+                }
+                else
+                    rootNodes.Add(nodeDictionary[access.ID]);
+            }
+
+            return rootNodes;
         }
     }
 }
---
+++ Presentation/Models/AccessNode.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json.Serialization;
+
+namespace AggregateVersions.Presentation.Models
+{
+    public class AccessNode
+    {
+        [JsonPropertyName("id")]
+        public string? ID { get; set; }
+
+        [JsonPropertyName("text")]
+        public string? DisplayName { get; set; }
+
+        [JsonPropertyName("children")]
+        public List<AccessNode>? Nodes { get; set; }
+
+        [JsonIgnore()]
+        public bool Children => Nodes != null && Nodes.Count != 0;
+
+    }
+}
